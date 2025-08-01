db1f6907e99168bacf48bfafa2a1e19bba79200e
Rhemjohn Dave <rdpitong@gmail.com>
2025-08-01 15:33:57 +0800

    fix slug on announcement

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -60,26 +60,6 @@ class AnnouncementController extends Controller
 
         $data = $request->only(['title', 'content', 'category_id']);
 
-        // Generate slug from title with proper cleaning
-        $cleanTitle = strip_tags($request->title);
-        $cleanTitle = html_entity_decode($cleanTitle, ENT_QUOTES, 'UTF-8');
-        $baseSlug = \Illuminate\Support\Str::slug($cleanTitle);
-
-        if (empty($baseSlug)) {
-            $baseSlug = 'announcement-' . time();
-        }
-
-        $slug = $baseSlug;
-        $counter = 1;
-
-        // Make sure slug is unique
-        while (\App\Models\Announcement::where('slug', $slug)->exists()) {
-            $slug = $baseSlug . '-' . $counter;
-            $counter++;
-        }
-
-        $data['slug'] = $slug;
-
         if ($request->hasFile('picture')) {
             $data['picture'] = $request->file('picture')->store('uploads', 'public');
         }
@@ -121,26 +101,9 @@ class AnnouncementController extends Controller
 
         $data = $request->only(['title', 'content', 'category_id']);
 
-        // Update slug if title changed
+        // Clear slug if title changed so model can regenerate it
         if ($announcement->title !== $request->title) {
-            $cleanTitle = strip_tags($request->title);
-            $cleanTitle = html_entity_decode($cleanTitle, ENT_QUOTES, 'UTF-8');
-            $baseSlug = \Illuminate\Support\Str::slug($cleanTitle);
-
-            if (empty($baseSlug)) {
-                $baseSlug = 'announcement-' . $announcement->id;
-            }
-
-            $slug = $baseSlug;
-            $counter = 1;
-
-            // Make sure slug is unique (excluding current announcement item)
-            while (\App\Models\Announcement::where('slug', $slug)->where('id', '!=', $announcement->id)->exists()) {
-                $slug = $baseSlug . '-' . $counter;
-                $counter++;
-            }
-
-            $data['slug'] = $slug;
+            $data['slug'] = null;
         }
 
         if ($request->hasFile('picture')) {
--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -18,13 +18,49 @@ class Announcement extends Model
 
         static::creating(function ($announcement) {
             if (empty($announcement->slug)) {
-                $announcement->slug = Str::slug($announcement->title);
+                // Generate slug from title with proper cleaning
+                $cleanTitle = strip_tags($announcement->title);
+                $cleanTitle = html_entity_decode($cleanTitle, ENT_QUOTES, 'UTF-8');
+                $baseSlug = Str::slug($cleanTitle);
+
+                if (empty($baseSlug)) {
+                    $baseSlug = 'announcement-' . time();
+                }
+
+                $slug = $baseSlug;
+                $counter = 1;
+
+                // Make sure slug is unique
+                while (static::where('slug', $slug)->exists()) {
+                    $slug = $baseSlug . '-' . $counter;
+                    $counter++;
+                }
+
+                $announcement->slug = $slug;
             }
         });
 
         static::updating(function ($announcement) {
             if ($announcement->isDirty('title') && empty($announcement->slug)) {
-                $announcement->slug = Str::slug($announcement->title);
+                // Generate slug from title with proper cleaning
+                $cleanTitle = strip_tags($announcement->title);
+                $cleanTitle = html_entity_decode($cleanTitle, ENT_QUOTES, 'UTF-8');
+                $baseSlug = Str::slug($cleanTitle);
+
+                if (empty($baseSlug)) {
+                    $baseSlug = 'announcement-' . $announcement->id;
+                }
+
+                $slug = $baseSlug;
+                $counter = 1;
+
+                // Make sure slug is unique (excluding current announcement item)
+                while (static::where('slug', $slug)->where('id', '!=', $announcement->id)->exists()) {
+                    $slug = $baseSlug . '-' . $counter;
+                    $counter++;
+                }
+
+                $announcement->slug = $slug;
             }
         });
     }
